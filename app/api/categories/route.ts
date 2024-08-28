ecf121db41c2b337286918dd9dbf2671aae0f379
shadowfeind <travestroy007@gmail.com>
2024-08-28 10:59:46 +0545

    api to get all category

---
+++ app/api/categories/route.ts
@@ -0,0 +1,29 @@
+import connectDB from "@/config/db/connect";
+import { auth } from "@/lib/auth";
+import Category from "@/models/category.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const { session } = await auth();
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await connectDB();
+
+    const categories = await Category.find({}).sort({ createdAt: 1 }).lean();
+
+    return NextResponse.json(
+      { success: true, data: categories },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return NextResponse.json(
+      { success: false, error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
