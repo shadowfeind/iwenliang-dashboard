d90332d9b72511112ec6da19b32905c76707f31a
shadowfeind <travestroy007@gmail.com>
2024-09-18 10:08:50 +0545

    file upload

--- config/db/connect.ts
+++ config/db/connect.ts
@@ -1,30 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let connected = false;
 
 const connectDB = async () => {
-  mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
   // If the database is already connected, don't connect again
   if (connected) {
-    console.log('MongoDB is already connected...');
+    console.log("MongoDB is already connected...");
     return;
   }
 
   // Ensure that MONGO_URI is defined
   const mongoUri = process.env.MONGO_URI;
   if (!mongoUri) {
-    throw new Error('MONGO_URI is not defined in the environment variables.');
+    throw new Error("MONGO_URI is not defined in the environment variables.");
   }
 
   // Connect to MongoDB
   try {
     await mongoose.connect(mongoUri);
     connected = true;
-    console.log('MongoDB connected...');
+    console.log("MongoDB connected...");
   } catch (error) {
     console.log(error);
-    throw new Error('MongoDB connection failed');
+    throw new Error("MongoDB connection failed");
   }
 };
 
